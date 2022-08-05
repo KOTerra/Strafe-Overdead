f41d64b121ad6fb752cf530ee3fa58e4bbdac57d
KOTerra <82443072+KOTerra@users.noreply.github.com>
2022-08-05 20:29:39 +0300

    made a gdx project

---
+++ StraferDeliberator/desktop/src/game/straferdeliberator/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package game.straferdeliberator;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import game.straferdeliberator.StraferDeliberator;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Strafer Deliberator");
+		new Lwjgl3Application(new StraferDeliberator(), config);
+	}
+}
