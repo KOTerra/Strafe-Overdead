27bee07549c9acd5a6e4135887bb0c52cb66f7dc
KOTerra <82443072+KOTerra@users.noreply.github.com>
2022-12-29 23:53:06 +0200

    #8 worked on getting preferences

--- StraferDeliberator/core/src/com/straferdeliberator/settings/Settings.java
+++ StraferDeliberator/core/src/com/straferdeliberator/settings/Settings.java
@@ -1,5 +1,14 @@
 package com.straferdeliberator.settings;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+
 public class Settings {
+	private final Preferences preferences = Gdx.app.getPreferences("strafer_preferences");
+
+	private String LANGUAGE = preferences.getString("LANGUAGE", "en");
 	
+	public static Preferences getPreferences() {
+		return Gdx.app.getPreferences("strafer_preferences");
+	}
 }
