6c1c0a9a8c90d76c6dea0fe8072cb3ae2a959af7
KOTerra <82443072+KOTerra@users.noreply.github.com>
2023-04-10 18:51:34 +0300

    #14 Started working on RenderingSystem

--- StrafeOverdead/core/src/com/strafergame/game/ecs/system/AnimationSystem.java
+++ StrafeOverdead/core/src/com/strafergame/game/ecs/system/AnimationSystem.java
@@ -3,10 +3,12 @@ package com.strafergame.game.ecs.system;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.strafergame.Strafer;
 import com.strafergame.game.ecs.ComponentMappers;
 import com.strafergame.game.ecs.component.AnimationComponent;
+import com.strafergame.game.ecs.component.EntityTypeComponent;
+import com.strafergame.graphics.AnimationProvider;
 
 public class AnimationSystem extends IteratingSystem {
 	public AnimationSystem() {
@@ -18,9 +20,11 @@ public class AnimationSystem extends IteratingSystem {
 		final AnimationComponent aniCmp = ComponentMappers.animation.get(entity);
 
 		aniCmp.timer += deltaTime;
-		TextureRegion currentFrame = aniCmp.animation.getKeyFrame(aniCmp.timer);
-		aniCmp.width = currentFrame.getRegionWidth() * Strafer.SCALE_FACTOR;
-		aniCmp.height = currentFrame.getRegionHeight() * Strafer.SCALE_FACTOR;
+
+		// aniCmp.animation = AnimationProvider.getAnimation(entity);
+		aniCmp.currentFrame = aniCmp.animation.getKeyFrame(aniCmp.timer, true);
+		aniCmp.width = aniCmp.currentFrame.getRegionWidth() * Strafer.SCALE_FACTOR;
+		aniCmp.height = aniCmp.currentFrame.getRegionHeight() * Strafer.SCALE_FACTOR;
 
 	}
 }
---
+++ StrafeOverdead/core/src/com/strafergame/game/ecs/system/render/ZComparator.java
@@ -0,0 +1,30 @@
+package com.strafergame.game.ecs.system.render;
+
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Entity;
+import com.strafergame.game.ecs.ComponentMappers;
+import com.strafergame.game.ecs.component.PositionComponent;
+
+import java.util.Comparator;
+
+public class ZComparator implements Comparator<Entity> {
+	private ComponentMapper<PositionComponent> posCmp;
+
+	public ZComparator() {
+		posCmp = ComponentMappers.position;
+	}
+
+	@Override
+	public int compare(Entity a, Entity b) {
+		float ay = posCmp.get(a).y;
+		float by = posCmp.get(b).y;
+		if (ay > by) {
+			return 1;
+		}
+		if (ay < by) {
+			return -1;
+		}
+		return 0;
+	}
+
+}
