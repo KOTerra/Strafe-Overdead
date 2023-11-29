96c0a1d3b3b1e1b4dd680b8bcfc8b94d31229b5b
KOTerra <michel69130@gmail.com>
2023-11-29 13:53:11 +0200

    #12 healthBar interpolation

--- StrafeOverdead/core/src/com/strafergame/game/ecs/system/combat/HealthSystem.java
+++ StrafeOverdead/core/src/com/strafergame/game/ecs/system/combat/HealthSystem.java
@@ -16,39 +16,39 @@ import com.strafergame.screens.GameScreen;
 
 public class HealthSystem extends IteratingSystem {
 
-	public HealthSystem(Box2DWorld box2dWorld) {
-		super(Family.all(HealthComponent.class, Box2dComponent.class).get());
-	}
+    public HealthSystem(Box2DWorld box2dWorld) {
+        super(Family.all(HealthComponent.class, Box2dComponent.class).get());
+    }
 
-	@Override
-	protected void processEntity(Entity entity, float deltaTime) {
-		Box2dComponent b2dCmp = ComponentMappers.box2d().get(entity);
-		HealthComponent hlthCmp = ComponentMappers.health().get(entity);
-		EntityTypeComponent ettCmp = ComponentMappers.entityType().get(entity);
-		AttackComponent attckCmp = AttackContactPair.getAttack(b2dCmp);
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        Box2dComponent b2dCmp = ComponentMappers.box2d().get(entity);
+        HealthComponent hlthCmp = ComponentMappers.health().get(entity);
+        EntityTypeComponent ettCmp = ComponentMappers.entityType().get(entity);
+        AttackComponent attckCmp = AttackContactPair.getAttack(b2dCmp);
 
-		if (attckCmp instanceof AttackComponent) {
-			if (attckCmp.owner != null && attckCmp.owner.equals(entity)) {
-				return;
-			}
-			ettCmp.entityState = EntityState.hit;
+        if (attckCmp instanceof AttackComponent) {
+            if (attckCmp.owner != null && attckCmp.owner.equals(entity)) {
+                return;
+            }
+            ettCmp.entityState = EntityState.hit;
 
-			hlthCmp.hitPoints -= attckCmp.damagePerSecond * deltaTime;
+            hlthCmp.hitPoints -= attckCmp.damagePerSecond * deltaTime;
 
-			if (hlthCmp.hitPoints <= 0) {
-				ettCmp.entityState = EntityState.death;
-			}
+            if (hlthCmp.hitPoints <= 0) {
+                ettCmp.entityState = EntityState.death;
+            }
 
-		}
-		if (ettCmp.entityState.equals(EntityState.death)){
-			if(!ettCmp.entityType.equals(EntityType.player)){
-				this.getEngine().removeEntity(entity);
-			}
-			else{
-				GameScreen.getInstance().showGameOverMenu();
-			}
-		}
+        }
+        if (ettCmp.entityState.equals(EntityState.death)) {
+            if (!ettCmp.entityType.equals(EntityType.player)) {
+                this.getEngine().removeEntity(entity);
+            } else {
+                GameScreen.getInstance().showGameOverMenu();
+            }
 
-	}
+        }
+
+    }
 
 }
--- StrafeOverdead/core/src/com/strafergame/game/ecs/system/player/HudSystem.java
+++ StrafeOverdead/core/src/com/strafergame/game/ecs/system/player/HudSystem.java
@@ -3,6 +3,9 @@ package com.strafergame.game.ecs.system.player;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.MathUtils;
+import com.kotcrab.vis.ui.widget.VisProgressBar;
 import com.strafergame.Strafer;
 import com.strafergame.game.ecs.ComponentMappers;
 import com.strafergame.game.ecs.component.HealthComponent;
@@ -10,14 +13,15 @@ import com.strafergame.game.ecs.component.PlayerComponent;
 
 public class HudSystem extends IteratingSystem {
 
-	public HudSystem() {
-		super(Family.all(PlayerComponent.class).get());
-	}
+    public HudSystem() {
+        super(Family.all(PlayerComponent.class).get());
+    }
 
-	@Override
-	protected void processEntity(Entity entity, float deltaTime) {
-		HealthComponent hlthCmp = ComponentMappers.health().get(entity);
-		Strafer.uiManager.getHud().getHealthBar().setValue(hlthCmp.hitPoints);
-	}
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        HealthComponent healthCmp = ComponentMappers.health().get(entity);
+        VisProgressBar healthBar = Strafer.uiManager.getHud().getHealthBar();
+        healthBar.setValue(Interpolation.linear.apply(healthBar.getValue(), healthCmp.hitPoints, .05f));
+    }
 
 }
