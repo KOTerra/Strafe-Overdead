1d1f047e9c9152f99f686f1caf3e0f09c19794e7
Stoica Mihai <82443072+KOTerra@users.noreply.github.com>
2024-08-12 13:17:16 +0300

    #25  SaveRecord retrieval method

--- StrafeOverdead/core/src/com/strafergame/game/ecs/system/save/SaveSystem.java
+++ StrafeOverdead/core/src/com/strafergame/game/ecs/system/save/SaveSystem.java
@@ -1,6 +1,8 @@
 package com.strafergame.game.ecs.system.save;
 
-import java.io.IOException;
+import com.strafergame.settings.Settings;
+
+import java.util.HashMap;
 
 public class SaveSystem {
     public static boolean suppressAutosave = false;
@@ -9,8 +11,21 @@ public class SaveSystem {
 
     public static Save getCurrentSave() {
         if (currentSave == null) {
-            currentSave = new Save();
+            String slot = Settings.getPreferences().getString("LAST_USED_SAVE_SLOT", "1");
+            String index = Settings.getPreferences().getString("LAST_SAVE_INDEX_ON_SLOT_" + slot, "0");//index on the given slot
+            currentSave = new Save(Integer.parseInt(slot), Integer.parseInt(index));
         }
         return currentSave;
+    }
+
+    public static <T> T retrieveFromRecords(String key) {
+        HashMap<String, Save.SaveRecord> records = SaveSystem.getCurrentSave().getRecords();
+        if (records != null && records.containsKey(key)) {
+            Save.SaveRecord<T> record = (Save.SaveRecord<T>) records.get(key);
+            if (record != null) {
+                return record.getObject();
+            }
+        }
+        return null;
     }
 }
