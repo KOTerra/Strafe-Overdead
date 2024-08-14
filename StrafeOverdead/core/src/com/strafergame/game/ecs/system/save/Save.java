b856420b479edc0ad0803f250987ff32159f3e34
Stoica Mihai <82443072+KOTerra@users.noreply.github.com>
2024-08-14 16:44:07 +0300

    #25 changed cleaner methods

--- StrafeOverdead/core/src/com/strafergame/game/ecs/system/save/Save.java
+++ StrafeOverdead/core/src/com/strafergame/game/ecs/system/save/Save.java
@@ -9,42 +9,40 @@ import com.strafergame.settings.Settings;
 
 import java.io.File;
 import java.io.IOException;
-import java.sql.Time;
 import java.time.Instant;
+import java.util.Date;
 import java.util.HashMap;
 
 public class Save extends Entity {
 
-    //    private int slotIndex;
-//    private int saveIndex;
     private String fileName;
     private FileHandle fileHandle;
     private File file;
+
     private boolean justCreated = false;
     private Instant created;
     private Instant lastSaved;
+
     private static final Json json = new Json();
 
     private HashMap<String, SaveRecord> records = new HashMap<>();
 
-    public <T> Save(int slotIndex, int saveIndex) {
-
-//        String slot = Settings.getPreferences().getString("LAST_USED_SAVE_SLOT", "1");
-//        slotIndex = Integer.parseInt(slot);
+    public Save(int slotIndex, int saveIndex) {
 
         Gdx.files.external(".strafedevs/saves/slot_" + slotIndex).mkdirs();
 
-//        String index = Settings.getPreferences().getString("LAST_SAVE_INDEX_ON_SLOT_" + slot, "0");//index on the given slot
-//        saveIndex = Integer.parseInt(index);// + 1;
         fileName = ".strafedevs/saves/slot_" + slotIndex + "/save_" + saveIndex + ".sav";  //+_snapshot_datehourtimesmth
         fileHandle = Gdx.files.external(fileName);
         file = fileHandle.file();
         try {
             justCreated = file.createNewFile();
+            if (justCreated) {
+                System.out.println("\nJust created save_" + slotIndex + "_" + saveIndex);
+            }
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
-        System.out.println(file.getAbsolutePath());
+        System.out.println(file.getAbsolutePath() + "\n");
         Settings.getPreferences().putString("LAST_SAVE_INDEX_ON_SLOT_" + slotIndex, Integer.toString(saveIndex));
         Settings.getPreferences().putString("LAST_USED_SAVE_SLOT", Integer.toString(slotIndex)).flush();
     }
@@ -76,13 +74,13 @@ public class Save extends Entity {
 
         lastSaved = Instant.now();
         register("LAST_SAVED_SECONDS", lastSaved.getEpochSecond(), Long.class);
-        System.out.println("\nSaved at: " + Time.from(lastSaved));
-        
+        System.out.println("\nSaved at: " + Date.from(lastSaved));
+
         fileHandle.writeString(json.toJson(records), false);
     }
 
 
-    public <T> void deserialize() {
+    public void deserialize() {
         records.clear();
         String jsonString = fileHandle.readString();
 
@@ -92,7 +90,7 @@ public class Save extends Entity {
                 created = Instant.ofEpochSecond(SaveSystem.retrieveFromRecords("FIRST_CREATED_SECONDS"));
 
                 Instant savedAt = Instant.ofEpochSecond(SaveSystem.retrieveFromRecords("LAST_SAVED_SECONDS"));
-                System.err.println("\nLoaded save from: " + Time.from(savedAt) + "\n");
+                System.err.println("\nLoaded save from: " + Date.from(savedAt) + "\n");
                 records.forEach((key, value) -> {
                     SaveRecord record = records.get(key);
                     // System.out.println("deser "+record.object);
@@ -131,7 +129,6 @@ public class Save extends Entity {
         public T getObject() {
             return object;
         }
-
 
         @Override
         public void write(Json json) {
